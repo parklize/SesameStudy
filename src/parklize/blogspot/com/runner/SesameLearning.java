7db66317370c4a11ae7cd58e20cb5ce5487248ac
parklize <parklize@gmail.com>
2014-09-03 15:21:08 +0100

    sesame

--- src/parklize/blogspot/com/runner/SesameLearning.java
+++ src/parklize/blogspot/com/runner/SesameLearning.java
@@ -38,18 +38,69 @@ public class SesameLearning {
      */
     public static void main(String[] args) {
         try {
+            String prefix = "PREFIX sioc:<http://rdfs.org/sioc/ns#>\n"
+                    + "PREFIX dc:<http://purl.org/dc/terms/>\n"
+                    + "PREFIX foaf:<http://xmlns.com/foaf/0.1/>\n"
+                    + "PREFIX og:<http://ogp.me/ns#>\n"
+                    + "PREFIX ogp:<http://opengraphprotocol.org/schema/>\n"
+                    + "PREFIX schema:<http://schema.org/>\n"
+                    + "PREFIX fn:<http://www.w3.org/2005/xpath-functions/>\n";
             RepositoryConnection con = SesameManager.repositoryConnect("C:\\Users\\GoFor2014\\Documents\\NetBeansProjects\\SesameStudy");
-//            SesameManager.insertNQDataToRepo(con, "C:\\sample.nq");
+//            SesameManager.insertNQDataToRepo(con, "C:\\Users\\GoFor2014\\Downloads\\socialdata.nq");
+//            SesameManager.clearRepository(con);
             String queryString = "SELECT (COUNT(*) AS ?count) WHERE {?x ?p ?y}";
+//            queryString = prefix+"SELECT (COUNT(*) AS ?count) WHERE {"
+//                    + "{?s dc:created ?t .} UNION "
+//                    + "{?s dc:date ?t .} UNION "
+//                    + "{?s dc:issued ?t .} UNION "
+//                    + "{?s schema:datePublished ?t .} UNION "
+//                    + "{?s schema:startDate ?t .} "
+//                    + "{?s <http://purl.org/rss/1.0/modules/content/encoded> ?c .} UNION "
+//                    + "{?s dc:title ?c .} UNION "
+//                    + "{?s og:description ?c .} UNION "
+//                    + "{?s schema:articleBody ?c .} UNION "
+//                    + "{?s sioc:content ?c .} UNION "
+//                    + "{?s schema:blogPosts ?c .} UNION "
+//                    + "{?s schema:blogPost ?c .} UNION "
+//                    + "{?s ogp:description ?s .} UNION "
+//                    + "{?s ogp:title ?s .} UNION "
+//                    + "{?s schema:articleSection ?s .} UNION "
+//                    + "{?s schema:keywords ?s .} UNION "
+//                    + "{?s schema:description ?s .} UNION "
+//                    + "{?s <http://purl.org/dc/elements/1.1/title> ?c .}"
+//                    + "{?s rdf:type sioc:Post .} UNION "
+//                    + "{?s rdf:type foaf:Document .} UNION "
+//                    + "{?s rdf:type sioc:Item .} UNION "
+//                    + "{?s rdf:type sioc:BlogPost .} "
+//                    + "FILTER (year(?t) = 2012 && CONTAINS(?c,\"gangnam\"))"
+//                    + "}";
+            queryString = prefix + "SELECT (COUNT(DISTINCT ?s) AS ?count) WHERE { "
+                    + "?s rdf:type ?type . "
+                    + "?s ?p1 ?content . "
+                    + "?s ?p2 ?time . "
+                    + "VALUES ?type {sioc:BlogPost sioc:Item sioc:Post foaf:Document} "
+                    + "VALUES ?p1 {dc:title og:description ogp:description ogp:title schema:articleBody sioc:content "
+                    + "schema:blogPosts schema:blogPost schema:articleSection schema:keywords "
+                    + "schema:description <http://purl.org/dc/elements/1.1/title> <http://purl.org/rss/1.0/modules/content/encoded>} "
+                    + "VALUES ?p2 {dc:created dc:date dc:issued schema:datePublished schema:startDate} "
+                    + "FILTER (year(?time) = 2012)"
+                    + "}";
             try {
                 TupleQuery tupleQuery = con.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
                 TupleQueryResult result = tupleQuery.evaluate();
                 while(result.hasNext()){
                     BindingSet bindingSet = result.next();
                     System.out.println(bindingSet.getValue("count"));
+//                    String content = bindingSet.getValue("c").toString().toLowerCase();
+//                    if(content.contains("gangnam") || content.contains(" psy ")){
+//                        System.out.println(bindingSet.getValue("s"));
+//                        System.out.print(bindingSet.getValue("time")+"-----");
+//                        System.out.println(bindingSet.getValue("content"));
+//                    }
                 }
             } catch (MalformedQueryException ex) {
                 Logger.getLogger(SesameLearning.class.getName()).log(Level.SEVERE, null, ex);
+                
             } catch (QueryEvaluationException ex) {
                 Logger.getLogger(SesameLearning.class.getName()).log(Level.SEVERE, null, ex);
             }
